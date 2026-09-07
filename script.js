1e98356b2f03d981c690a03bbb9e437e79fbf8b0
green-nk <80184132+green-nk@users.noreply.github.com>
2026-09-07 23:27:15 +0700

    Add sketch grid
    Given `numSquaresPerSide` and `PadSize`, we can calculate the squareSize
    dynamically.

---
+++ script.js
@@ -0,0 +1,20 @@
+const numSquaresPerSide = 16;
+const numSquaresTotal = numSquaresPerSide * numSquaresPerSide;
+
+const pad = document.querySelector(".pad");
+createGrid();
+
+function createGrid() {
+    const padSize = pad.clientWidth;
+    const squareSize = padSize / numSquaresPerSide;
+
+    for (let i = 0; i < numSquaresTotal; i++) {
+        const cell = document.createElement("div");
+    
+        cell.classList.add("cell");
+        cell.style.width = `${squareSize}px`;
+        cell.style.height = `${squareSize}px`;
+    
+        pad.appendChild(cell);
+    }
+};
