8861a540f9724cee5a838248217dbe15d7f237b1
green-nk <80184132+green-nk@users.noreply.github.com>
2026-09-11 13:39:15 +0700

    Add changing grid size functionality

--- script.js
+++ script.js
@@ -1,32 +1,39 @@
 let numSquaresPerSide = 16;
-const numSquaresTotal = numSquaresPerSide * numSquaresPerSide;
+let numSquaresTotal = numSquaresPerSide * numSquaresPerSide;
 
 const pad = document.querySelector(".pad");
 createGrid();
 
-const cells = document.querySelectorAll(".cell");
-cells.forEach((cell) => {
-    cell.addEventListener("mouseenter", () => {
-        cell.classList.add("hovered");
-    });
-});
-
 const button = document.querySelector("button");
 button.addEventListener("click", () => {
-    numSquaresPerSide = +prompt("Grid Size?");
+    numSquaresPerSide = +prompt("Enter Grid Size (Max 100)");
+    numSquaresTotal = numSquaresPerSide * numSquaresPerSide;
+    
+    removeGrid();
+    createGrid();
 });
 
 function createGrid() {
     const padSize = pad.clientWidth;
     const squareSize = padSize / numSquaresPerSide;
-
+    
     for (let i = 0; i < numSquaresTotal; i++) {
         const cell = document.createElement("div");
-    
+        
         cell.classList.add("cell");
         cell.style.width = `${squareSize}px`;
         cell.style.height = `${squareSize}px`;
-    
+        
+        cell.addEventListener("mouseenter", () => {
+            cell.classList.add("hovered");
+        });
+        
         pad.appendChild(cell);
+    }
+};
+
+function removeGrid() {
+    while (pad.firstChild) {
+        pad.removeChild(pad.firstChild);
     }
 };
